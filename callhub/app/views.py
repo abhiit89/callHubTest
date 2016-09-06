83cbc6a29f2ee920838e7b40d18ef36c87034a14
abhinav <abhi.aec89@gmail.com>
2016-09-06 21:15:46 +0530

    refactoring

--- callhub/app/views.py
+++ callhub/app/views.py
@@ -1,13 +1,17 @@
 import datetime
-from math import sqrt
+
 from django.http import HttpResponse
 from django.shortcuts import render
+
 from .forms import SubmitFibonacci
 
-global f; f = [0,1,1]
+global fibonacci_sequence
+fibonacci_sequence = [0, 1, 1]
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the Fibonacci Page.")
+
 
 def save_fibonacci(request):
     start_time = datetime.datetime.now().time().strftime('%H:%M:%S')
@@ -17,21 +21,22 @@ def save_fibonacci(request):
             index = form.cleaned_data['index']
             response_data = {}
             response_data["index"] = int(index)
-            response_data["result"] = fibonacci(int(index))
+            response_data["result"] = fibonacci_calculator(int(index))
             end_time = datetime.datetime.now().time().strftime('%H:%M:%S')
-            response_data["elapsed"] = (datetime.datetime.strptime(end_time,'%H:%M:%S') - datetime.datetime.strptime(start_time,'%H:%M:%S'))
-            return render(request, 'fibonacci.html', {'fibonacci': response_data})
+            response_data["elapsed"] = (
+            datetime.datetime.strptime(end_time, '%H:%M:%S') - datetime.datetime.strptime(start_time, '%H:%M:%S'))
+            return render(request, 'fibonacci_calculator.html', {'fibonacci_calculator': response_data})
     else:
         form = SubmitFibonacci()
 
     return render(request, 'index.html', {'form': form})
 
-def fibonacci(n):
-    #import pdb;pdb.set_trace()
+
+def fibonacci_calculator(n):
     try:
-        if f[n]:
-            return f[n - 1]
+        if fibonacci_sequence[n]:
+            return fibonacci_sequence[n - 1]
     except:
         for i in xrange(3, n):
-            f.append(f[i - 1] + f[i - 2])
-        return f[-1]
+            fibonacci_sequence.append(fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2])
+        return fibonacci_sequence[-1]
